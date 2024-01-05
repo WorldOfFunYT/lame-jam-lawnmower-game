34cb6044b70445d619dcec169e869a2eb1a1602d
WorldOfFunYT <czarczynskiszymon@gmail.com>
2024-01-05 21:31:43 +0000

    Started working on a menu and added placeholder assets

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ class Game:
     self.height = 480
     pygame.init()
 
+    self.currentMenu = 'game'
+
     self.screen = pygame.display.set_mode((self.width * SCREEN_SIZE_MULT, self.height * SCREEN_SIZE_MULT))
     self.display = pygame.Surface((self.width, self.height))
 
@@ -41,105 +43,124 @@ class Game:
     self.progressbar = Progressbar([320, 24], 320, 10)
 
   def run(self):
+    selected = 0
     while True:
       self.display.fill((0, 0, 10))
 
-      for event in pygame.event.get():
-        match event.type:
-          case pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-            break
-          case pygame.JOYDEVICEADDED:
-            joy = pygame.joystick.Joystick(event.device_index)
-            self.joysticks[joy.get_instance_id()] = joy
-            break
-          case pygame.JOYDEVICEREMOVED:
-            del self.joysticks[event.instance_id]
-            break
+      match self.currentMenu:
+        case "playerSelect":
+          for event in pygame.event.get():
+            match event.type:
+              case pygame.K_RIGHT:
+                selected = (selected + 1) % 3
+                break
+              case pygame.K_LEFT:
+                selected -= 1
+                if selected < 0:
+                  selected = 2
+                break
+              case pygame.K_z:
+                self.currentMenu = "game"
+                break
+          print("HIIIII")
+          
+        case "game":
+          for event in pygame.event.get():
+            match event.type:
+              case pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                break
+              case pygame.JOYDEVICEADDED:
+                joy = pygame.joystick.Joystick(event.device_index)
+                self.joysticks[joy.get_instance_id()] = joy
+                break
+              case pygame.JOYDEVICEREMOVED:
+                del self.joysticks[event.instance_id]
+                break
 
-      
-      for player in self.players:
-        if player.controllerType == "keyboard":
-          keys = pygame.key.get_pressed()
-          rotationV = 0
-          movement = 0
-          if keys[player.controlScheme[2]]:
-            rotationV = -1
-          if keys[player.controlScheme[3]]:
-            rotationV = 1
-          if keys[player.controlScheme[0]]:
-            movement = 1
-          if keys[player.controlScheme[1]]:
-            movement = -1
-          player.movement += movement
-          player.rotationDirection += rotationV
-
-      for joystick in self.joysticks.values():
-        jid = joystick.get_instance_id()
-
-        for i in range(joystick.get_numaxes()):
-          axis = joystick.get_axis(i)
-          if abs(axis) <= 0.1:
-            axis = 0
-          if i == 0:
-            for player in self.players:
-              if player.controllerType == "controller" and player.controllerId == jid:
-                player.rotationDirection = axis
-      
-        for i in range(joystick.get_numbuttons()):
-          button = joystick.get_button(i)
+          
           for player in self.players:
-            if player.controllerType == "controller" and player.controllerId == jid:
+            if player.controllerType == "keyboard":
+              keys = pygame.key.get_pressed()
+              rotationV = 0
+              movement = 0
+              if keys[player.controlScheme[2]]:
+                rotationV = -1
+              if keys[player.controlScheme[3]]:
+                rotationV = 1
+              if keys[player.controlScheme[0]]:
+                movement = 1
+              if keys[player.controlScheme[1]]:
+                movement = -1
+              player.movement += movement
+              player.rotationDirection += rotationV
+
+          for joystick in self.joysticks.values():
+            jid = joystick.get_instance_id()
+
+            for i in range(joystick.get_numaxes()):
+              axis = joystick.get_axis(i)
+              if abs(axis) <= 0.1:
+                axis = 0
               if i == 0:
-                player.movement += button
-              if i == 1:
-                player.movement += button * -1
-      renderScrolls = [[0, 0]] * len(self.players)
-      
-      for i, player in enumerate(self.players):
-        player.update(self.gridWidth * self.cellSize, self.gridHeight * self.cellSize) #Position rotation 
+                for player in self.players:
+                  if player.controllerType == "controller" and player.controllerId == jid:
+                    player.rotationDirection = axis
+          
+            for i in range(joystick.get_numbuttons()):
+              button = joystick.get_button(i)
+              for player in self.players:
+                if player.controllerType == "controller" and player.controllerId == jid:
+                  if i == 0:
+                    player.movement += button
+                  if i == 1:
+                    player.movement += button * -1
+          renderScrolls = [[0, 0]] * len(self.players)
+          
+          for i, player in enumerate(self.players):
+            player.update(self.gridWidth * self.cellSize, self.gridHeight * self.cellSize) #Position rotation 
 
-        team = player.team # Set team colour for grid mapping
-        gridX, gridY = int(player.position[0]) // self.cellSize, int(player.position[1]) // self.cellSize
-        if 0 <= gridX < self.gridWidth and 0 <= gridY < self.gridHeight and self.grid[gridY][gridX] == 0:
-          self.grid[gridY][gridX] = team # Set grid colour
+            team = player.team # Set team colour for grid mapping
+            gridX, gridY = int(player.position[0]) // self.cellSize, int(player.position[1]) // self.cellSize
+            if 0 <= gridX < self.gridWidth and 0 <= gridY < self.gridHeight and self.grid[gridY][gridX] == 0:
+              self.grid[gridY][gridX] = team # Set grid colour
 
-        
-        self.camOffsets[i][0] = player.getCenter()[0] - self.cameras[i].get_width() / 2 # 
-        self.camOffsets[i][1] = player.getCenter()[1] - self.cameras[i].get_height() / 2
-        renderScrolls[i] = (int(self.camOffsets[i][0]), int(self.camOffsets[i][1]))
+            
+            self.camOffsets[i][0] = player.getCenter()[0] - self.cameras[i].get_width() / 2 # 
+            self.camOffsets[i][1] = player.getCenter()[1] - self.cameras[i].get_height() / 2
+            renderScrolls[i] = (int(self.camOffsets[i][0]), int(self.camOffsets[i][1]))
 
-      for i, camera in enumerate(self.cameras):
-        
-        camera.fill((0, 10, 0))
-        for y in range(max(renderScrolls[i][1] // self.cellSize, 0), min((renderScrolls[i][1] + camera.get_height()) // self.cellSize + 1, len(self.grid))):
-          for x in range(max(renderScrolls[i][0] // self.cellSize, 0), min((renderScrolls[i][0] + camera.get_width()) // self.cellSize + 1, len(self.grid[y]))):
-            colour = (0, 0, 0)
-            if self.grid[y][x] == 1:
-              colour = (0, 0, 128)
-            if self.grid[y][x] == 2:
-              colour = (128, 0, 0)
-            pygame.draw.rect(camera, colour, (x * self.cellSize - renderScrolls[i][0], y * self.cellSize - renderScrolls[i][1], self.cellSize, self.cellSize))
+          for i, camera in enumerate(self.cameras):
+            
+            camera.fill((0, 10, 0))
+            for y in range(max(renderScrolls[i][1] // self.cellSize, 0), min((renderScrolls[i][1] + camera.get_height()) // self.cellSize + 1, len(self.grid))):
+              for x in range(max(renderScrolls[i][0] // self.cellSize, 0), min((renderScrolls[i][0] + camera.get_width()) // self.cellSize + 1, len(self.grid[y]))):
+                colour = (0, 0, 0)
+                if self.grid[y][x] == 1:
+                  colour = (0, 0, 128)
+                if self.grid[y][x] == 2:
+                  colour = (128, 0, 0)
+                pygame.draw.rect(camera, colour, (x * self.cellSize - renderScrolls[i][0], y * self.cellSize - renderScrolls[i][1], self.cellSize, self.cellSize))
 
 
-        for player in self.players:
-          player.render(self.cameras[i], renderScrolls[i])
-        
-        self.display.blit(camera, (self.display.get_width() // 2 if (i + 1) % 2 == 0 else 0, self.display.get_height() // 2 if (i + 1) > 2 else 0))
+            for player in self.players:
+              player.render(self.cameras[i], renderScrolls[i])
+            
+            self.display.blit(camera, (self.display.get_width() // 2 if (i + 1) % 2 == 0 else 0, self.display.get_height() // 2 if (i + 1) > 2 else 0))
 
-      playerGridCoordinates = [[player.team, (int(player.position[0]) // self.cellSize, int(player.position[1]) // self.cellSize)] for player in self.players]
+          playerGridCoordinates = [[player.team, (int(player.position[0]) // self.cellSize, int(player.position[1]) // self.cellSize)] for player in self.players]
 
-      self.minimap.render(self.display, playerGridCoordinates)
+          self.minimap.render(self.display, playerGridCoordinates)
 
-      team1Percent = len([item for row in self.grid for item in row if item == 1]) / (self.gridWidth * self.gridHeight)
-      team2Percent = len([item for row in self.grid for item in row if item == 2]) / (self.gridWidth * self.gridHeight)
-      
-      self.progressbar.render(self.display, team1Percent, team2Percent)
+          team1Percent = len([item for row in self.grid for item in row if item == 1]) / (self.gridWidth * self.gridHeight)
+          team2Percent = len([item for row in self.grid for item in row if item == 2]) / (self.gridWidth * self.gridHeight)
+          
+          self.progressbar.render(self.display, team1Percent, team2Percent)
+          print(f'{round(team1Percent * 100, 2)}%-{round(team2Percent * 100, 2)}%') 
 
       self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
       pygame.display.update()
-      print(f'{round(team1Percent * 100, 2)}%-{round(team2Percent * 100, 2)}%') 
       # print(self.clock.get_fps())
       self.clock.tick(60)
 
