7a6ee5d2793556a31035f4042c804913dca648cb
WorldOfFunYT <czarczynskiszymon@gmail.com>
2024-01-05 16:00:28 +0000

    Added multiple players and cameras, and a grid

--- main.py
+++ main.py
@@ -23,11 +23,23 @@ class Game:
 
     self.joysticks = {}
 
-    self.player1 = Player()
+    self.players = [
+      Player(spawnCoords=[0, 0]), 
+      Player(controllerType="keyboard", team=2, spawnCoords=[100, 100]),
+      Player(controllerType="keyboard", team=1, spawnCoords=[-150, 100])
+      ]
+    # self.players = [Player()]
+    cameraSize = (
+      self.width if len(self.players) == 1 else self.width/2, 
+      self.height if len(self.players) <= 2 else self.height / 2 
+      )
+    self.cameras = [pygame.Surface(cameraSize) for _ in self.players]
+    self.camOffsets = [[0, 0]] * len(self.players)
 
   def run(self):
     while True:
       self.display.fill((0, 0, 10))
+      
 
       for event in pygame.event.get():
         match event.type:
@@ -43,6 +55,23 @@ class Game:
             del self.joysticks[event.instance_id]
             break
 
+      
+      for player in self.players:
+        if player.controllerType == "keyboard":
+          keys = pygame.key.get_pressed()
+          rotationV = 0
+          movement = 0
+          if keys[pygame.K_LEFT]:
+            rotationV = -1
+          if keys[pygame.K_RIGHT]:
+            rotationV = 1
+          if keys[pygame.K_UP]:
+            movement = 1
+          if keys[pygame.K_DOWN]:
+            movement = -1
+          player.movement += movement
+          player.rotationDirection += rotationV
+
       for joystick in self.joysticks.values():
         jid = joystick.get_instance_id()
 
@@ -51,31 +80,52 @@ class Game:
           if abs(axis) <= 0.1:
             axis = 0
           if i == 0:
-            self.player1.rotationDirection = axis
+            for player in self.players:
+              if player.controllerType == "controller" and player.controllerId == jid:
+                player.rotationDirection = axis
       
         for i in range(joystick.get_numbuttons()):
           button = joystick.get_button(i)
-          if i == 0:
-            self.player1.movement += button
-          if i == 1:
-            self.player1.movement += button * -1
+          for player in self.players:
+            if player.controllerType == "controller" and player.controllerId == jid:
+              if i == 0:
+                player.movement += button
+              if i == 1:
+                player.movement += button * -1
+      renderScrolls = [[0, 0]] * len(self.players)
       
-      self.player1.update()
+      for i, player in enumerate(self.players):
+        player.update() #Position rotation 
 
-      gridX, gridY = int(self.player1.position[0]) // self.cellSize, int(self.player1.position[1]) // self.cellSize
-      if 0 <= gridX < self.gridWidth and 0 <= gridY < self.gridHeight:
-        self.grid[gridY][gridX] = 1
+        team = player.team # Set team colour for grid mapping
+        gridX, gridY = int(player.position[0]) // self.cellSize, int(player.position[1]) // self.cellSize
+        if 0 <= gridX < self.gridWidth and 0 <= gridY < self.gridHeight:
+          self.grid[gridY][gridX] = team # Set grid colour
 
-      for y in range(self.gridHeight):
-        for x in range(self.gridWidth):
-          colour = (0, 0, 0)
-          if self.grid[y][x] == 1:
-            colour = (0, 0, 128)
-          if self.grid[y][x] == 2:
-            colour = (128, 0, 0)
-          pygame.draw.rect(self.display, colour, (x * self.cellSize, y * self.cellSize, self.cellSize, self.cellSize))
+        
+        self.camOffsets[i][0] = player.getCenter()[0] - self.cameras[i].get_width() / 2 # 
+        self.camOffsets[i][1] = player.getCenter()[1] - self.cameras[i].get_height() / 2
+        renderScrolls[i] = (int(self.camOffsets[i][0]), int(self.camOffsets[i][1]))
 
-      self.player1.render(self.display)
+      for i, camera in enumerate(self.cameras):
+        
+        camera.fill((0, 10, 0))
+        for y in range(self.gridHeight):
+          for x in range(self.gridWidth):
+            colour = (0, 0, 0)
+            if self.grid[y][x] == 1:
+              colour = (0, 0, 128)
+            if self.grid[y][x] == 2:
+              colour = (128, 0, 0)
+            pygame.draw.rect(camera, colour, (x * self.cellSize - renderScrolls[i][0], y * self.cellSize - renderScrolls[i][1], self.cellSize, self.cellSize))
+
+
+        for player in self.players:
+          print(renderScrolls[i])
+          player.render(self.cameras[i], renderScrolls[i])
+        
+        self.display.blit(camera, (self.display.get_width() // 2 if (i + 1) % 2 == 0 else 0, self.display.get_height() // 2 if (i + 1) > 2 else 0))
+
 
       self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
       pygame.display.update()
--- scripts/player.py
+++ scripts/player.py
@@ -2,14 +2,19 @@ import pygame
 import math
 
 class Player:
-  def __init__(self, spawnCoords=(100, 100), team="blue", rotation=0):
+  def __init__(self, spawnCoords=(100, 100), team=1, rotation=0, controllerType="controller", controllerId=0):
     self.spawnCoords = spawnCoords
     self.position = spawnCoords
     self.team = team
+
+    self.controllerType = controllerType
+    if controllerType == "controller":
+      self.controllerId = controllerId
+
     match team:
-      case "blue":
+      case 1:
         self.colour = (30, 150, 220)
-      case "red":
+      case 2:
         self.colour = (220, 30, 30)
     
     self.rotation = rotation
@@ -23,14 +28,17 @@ class Player:
     self.sprite = pygame.Surface((20, 20)).convert_alpha()
     self.sprite.fill(self.colour)
 
-  def update(self):
-    self.rotationVelocity += self.rotationDirection * 0.1
-    self.rotationVelocity = max(min(self.rotationVelocity, 4), -4)
-    self.rotation -= self.rotationVelocity * 2
+  def getCenter(self):
+    return (self.position[0] + self.sprite.get_width() / 2, self.position[1] + self.sprite.get_height() / 2)
 
+  def update(self):
     self.movement *= -1
     self.velocity += self.movement * 0.1
     self.velocity = max(min(self.velocity, 3), -3)
+
+    self.rotationVelocity += self.rotationDirection * 0.1 * abs(self.velocity / 3)
+    self.rotationVelocity = max(min(self.rotationVelocity, 4), -4)
+    self.rotation -= self.rotationVelocity * 2
 
     theta = math.radians(self.rotation) * -1
 
@@ -39,18 +47,21 @@ class Player:
     self.position = (self.position[0] + dx, self.position[1] + dy)
 
     if self.movement == 0:
-      if self.velocity > 0.01:
-        self.velocity *= 0.95
+      if abs(self.velocity) > 0.00001:
+        self.velocity *= 0.98
       else:
         self.velocity = 0
     self.movement = 0
 
     if self.rotationDirection == 0:
-      self.rotationVelocity *= 0.90
+      if abs(self.rotationVelocity > 0.01):
+        self.rotationVelocity *= 0.90
+      else:
+        self.rotationVelocity = 0
     self.rotationDirection = 0
-
-    print(self.velocity, self.rotationVelocity)
   
-  def render(self, surf):
+  def render(self, surf, offset=(0, 0)):
     rotated_surf = pygame.transform.rotate(self.sprite, self.rotation)
-    surf.blit(rotated_surf, (self.position[0] - rotated_surf.get_width() / 2, self.position[1] - rotated_surf.get_height() / 2))
+    surf.blit(rotated_surf, (
+      self.position[0] - rotated_surf.get_width() / 2 - offset[0], 
+      self.position[1] - rotated_surf.get_height() / 2 - offset[1]))
